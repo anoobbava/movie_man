a2c2496cced80e5404e16521dff2fc9d2405788a
Anoob Bava <anoob.bava@gmail.com>
2020-07-24 20:46:07 +0530

    Removed the logic from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,17 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'components/trending_movies.dart';
-
-Future<Album> fetchAlbum() async {
-  final String keyVar = DotEnv().env['MOVIE_KEY'];
-  final response =
-      await http.get("https://api.themoviedb.org/3/movie/551?api_key=$keyVar");
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final String title;
-
-  Album({this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      title: json['original_title'],
-    );
-  }
-}
 
 Future main() async {
   await DotEnv().load('.env');
   runApp(MyApp());
 }
-// runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Data Example',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
@@ -72,20 +23,6 @@ class _MyAppState extends State<MyApp> {
               ),
               Container(
                 child: TrendingMovies(),
-              ),
-              Container(
-                child: FutureBuilder<Album>(
-                  future: futureAlbum,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Text(snapshot.data.title);
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-                    // By default, show a loading spinner.
-                    return CircularProgressIndicator();
-                  },
-                ),
               ),
             ],
           ),
